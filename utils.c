4e5da0206b7e825f283191ab55795c80a5bbde24
esraa <82068115+esraaelsharqawy98@users.noreply.github.com>
2023-03-31 23:57:03 +0200

    update

--- utils.c
+++ utils.c
@@ -3,7 +3,6 @@
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
- *
  * Return: 1 if c is printable, 0 otherwise
  */
 int is_printable(char c)
@@ -40,7 +39,6 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 /**
  * is_digit - Verifies if a char is a digit
  * @c: Char to be evaluated
- *
  * Return: 1 if c is a digit, 0 otherwise
  */
 int is_digit(char c)
@@ -72,7 +70,6 @@ long int convert_size_number(long int num, int size)
  * convert_size_unsgnd - Casts a number to the specified size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
- *
  * Return: Casted value of num
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
